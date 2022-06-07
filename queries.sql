e3ad35fb2c064daa48c113d8f3b2d72122ece87b
sainingo <nickmentor421@gmail.com>
2022-06-08 00:12:32 +0300

    Query and update animals table

--- queries.sql
+++ queries.sql
@@ -15,3 +15,29 @@ SELECT * from animals WHERE neutered = 'true';
 SELECT * from animals WHERE name = 'Gabumon';
 
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species = NULL;
+DELETE FROM animals;
+
+ROLLBACK BeforeDelete;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+-- SAVE POINT
+
+-- UPDATE ALL ANIMALS WEIGHT BY MULTIPLY BY -1
+UPDATE animals SET weight_kg = (weight_kg * (-1));
+
+ROLLBACK SAVEPOINT;
+
+-- UPDATE ALL ANIMALS WEIGHTS WHICH ARE NEGATIVE AFTER WEIGHT MULTIPLIED BY -1
+UPDATE animals SET weight_kg = (weight_kg * -1)  WHERE weight_kg < 0;
+
+COMMIT;
+
+-- HOW MANY ANIMALS ARE THERE?
+SELECT COUNT(*) FROM animals;
+
+-- ANIMALS WHO HAVE NEVER TRIED TO ESCAPE
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
